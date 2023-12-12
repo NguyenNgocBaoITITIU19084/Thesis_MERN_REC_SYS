ca73dfa4e746403fb8f8eca4ae361995c10a7fba
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-12 14:22:37 +0700

    categories layouts

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/layout/Header";
 import Hero from "../components/Route/Hero/Hero";
+import Category from "../components/Route/Category/Category.jsx";
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
+      <Category />
     </div>
   );
 };
