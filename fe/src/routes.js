181b2604534507618dfcf4933be5cbfc67ce2cb1
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-12 14:40:52 +0700

    product card and best deal feature layout

--- fe/src/routes.js
+++
@@ -1,5 +0,0 @@
-import LoginPage from "./pages/Login/LoginPage";
-import SignUp from "./pages/SignUp/SignUp";
-import Home from "./pages/Home/Home";
-
-export { LoginPage, SignUp, Home };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/layout/Header";
 import Hero from "../components/Route/Hero/Hero";
 import Category from "../components/Route/Category/Category.jsx";
+import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Hero />
       <Category />
+      <BestDeals />
     </div>
   );
 };
