2ecb61885c3f841ef0cf42b2033ea85466d8e702
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-13 11:55:43 +0700

    faq page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignUpPage, HomePage } from "./Routes.js";
+import { LoginPage, SignUpPage, HomePage, FAQPage } from "./Routes.js";
 const App = () => {
   return (
     <BrowserRouter>
@@ -8,6 +8,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/faq" element={<FAQPage/>} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,4 +1,5 @@
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
-export { LoginPage, SignUpPage, HomePage };
+import FAQPage from "./pages/FAQPage.jsx";
+export { LoginPage, SignUpPage, HomePage, FAQPage };
