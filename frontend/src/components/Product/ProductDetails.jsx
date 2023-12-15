cf1a4752b09b78646afa4d73248c614a96ab5efb
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-15 14:58:37 +0700

    product details paage

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   AiFillHeart,
   AiOutlineHeart,
@@ -111,11 +111,166 @@ const ProductDetails = ({ data }) => {
                     )}
                   </div>
                 </div>
+                <div
+                  className={`${styles.button} !mt-6 !rounded !h-11 flex items-center`}
+                >
+                  <span className="text-white flex items-center">
+                    Add to cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
+                </div>
+                <div className="flex items-center pt-8">
+                  <Link to={`/shop/preview/${data?.shop._id}`}>
+                    <img
+                      src={`${data.shop.shop_avatar.url}`}
+                      alt=""
+                      className="w-[50px] h-[50px] rounded-full mr-2"
+                    />
+                  </Link>
+                  <div className="pr-8">
+                    <Link to={`/shop/preview/${data?.shop._id}`}>
+                      <h3 className={`${styles.shop_name} pb-1 pt-1`}>
+                        {data.shop.name}
+                      </h3>
+                    </Link>
+                    <h5 className="pb-3 text-[15px]">
+                      ({data.shop.rating})Ratings
+                    </h5>
+                  </div>
+                  <div
+                    className={`${styles.button} bg-[#6443d1] mt-4 !rounded !h-11`}
+                    // onClick={handleMessageSubmit}
+                  >
+                    <span className="text-white flex items-center">
+                      Send Message <AiOutlineMessage className="ml-1" />
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <ProductDetailsInfor />
         </div>
       ) : null}
+    </div>
+  );
+};
+
+const ProductDetailsInfor = ({ data }) => {
+  const [active, setActive] = useState(1);
+  return (
+    <div className="bg-[#f5f6fb] px-3 800px:px-10 py-2 rounded">
+      <div className="w-full flex justify-between border-b pt-10 pb-2">
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(1)}
+          >
+            Product Details
+          </h5>
+          {active === 1 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(2)}
+          >
+            Product Reviews
+          </h5>
+          {active === 2 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(3)}
+          >
+            Seller Information
+          </h5>
+          {active === 3 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+      </div>
+      {active === 1 ? (
+        <>
+          <p className="py-2 text-[18px] leading-8 pb-10 whitespace-pre-line">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+            velit repudiandae asperiores similique dolorum quia quis repellat
+            animi eveniet aut? Architecto inventore dolorum harum. Quisquam
+            quibusdam possimus soluta. Sunt, odit!
+          </p>
+          <p className="py-2 text-[18px] leading-8 pb-10 whitespace-pre-line">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+            velit repudiandae asperiores similique dolorum quia quis repellat
+            animi eveniet aut? Architecto inventore dolorum harum. Quisquam
+            quibusdam possimus soluta. Sunt, odit!
+          </p>
+        </>
+      ) : null}
+      {active === 2 ? (
+        <div className="w-full min-h-[40vh] flex flex-col items-center py-3 overflow-y-scroll">
+          <p>No Review yet!</p>
+        </div>
+      ) : null}
+      {active === 3 && (
+        <div className="w-full block 800px:flex p-5">
+          <div className="w-full 800px:w-[50%]">
+            <Link to={`/shop/preview/${data?.shop._id}`}>
+              <div className="flex items-center">
+                <img
+                  src={`${data?.shop?.shop_avatar.url}`}
+                  className="w-[50px] h-[50px] rounded-full"
+                  alt=""
+                />
+                <div className="pl-3">
+                  <h3 className={`${styles.shop_name}`}>BAo nguyen</h3>
+                  <h5 className="pb-2 text-[15px]">
+                    ({data?.shop?.rating}/5) Ratings
+                  </h5>
+                </div>
+              </div>
+            </Link>
+            <p className="pt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ullam
+              illum possimus explicabo dolorem eveniet dolore aut obcaecati
+              quibusdam modi architecto iure eos, impedit consequuntur ipsa quod
+              laboriosam quos voluptatum?
+            </p>
+          </div>
+          <div className="w-full 800px:w-[50%] mt-5 800px:mt-0 800px:flex flex-col items-end">
+            <div className="text-left">
+              <h5 className="font-[600]">
+                Joined on:{" "}
+                <span className="font-[500]">
+                  {data?.shop?.createdAt?.slice(0, 10)}
+                </span>
+              </h5>
+              <h5 className="font-[600] pt-3">
+                Total Products: <span className="font-[500]">123123</span>
+              </h5>
+              <h5 className="font-[600] pt-3">
+                Total Reviews: <span className="font-[500]">123123</span>
+              </h5>
+              <Link to="/">
+                <div
+                  className={`${styles.button} !rounded-[4px] !h-[39.5px] mt-3`}
+                >
+                  <h4 className="text-white">Visit Shop</h4>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
