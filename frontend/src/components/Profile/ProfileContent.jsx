e72292ce9841cfbb011533f4d66621bf3ae64261
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-15 16:23:55 +0700

    track order

--- frontend/src/components/Profile/ProfileContent.jsx
+++ frontend/src/components/Profile/ProfileContent.jsx
@@ -129,6 +129,12 @@ const ProfileContent = ({ active }) => {
           <AllRefundOrders />
         </div>
       )}
+      {/* Track order */}
+      {active === 5 && (
+        <div>
+          <TrackOrder />
+        </div>
+      )}
     </div>
   );
 };
@@ -324,6 +330,101 @@ const AllRefundOrders = () => {
         pageSize={10}
         autoHeight
         disableSelectionOnClick
+      />
+    </div>
+  );
+};
+const TrackOrder = () => {
+  // const { user } = useSelector((state) => state.user);
+  // const { orders } = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+  const orders = [
+    {
+      _id: "13123132213",
+      orderItems: [
+        {
+          name: "Iphone 14 promax",
+        },
+      ],
+      totalPrice: 120,
+      orderStatus: "Processing refund",
+    },
+  ];
+  // useEffect(() => {
+  //   dispatch(getAllOrdersOfUser(user._id));
+  // }, []);
+
+  const columns = [
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "status") === "Delivered"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/user/track/order/${params.id}`}>
+              <Button>
+                <MdTrackChanges size={20} />
+              </Button>
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+
+  const row = [];
+
+  orders &&
+    orders.forEach((item) => {
+      row.push({
+        id: item?._id,
+        itemsQty: item?.orderItems.length,
+        total: "US$ " + item?.totalPrice,
+        status: item?.orderStatus,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSize={10}
+        disableSelectionOnClick
+        autoHeight
       />
     </div>
   );
