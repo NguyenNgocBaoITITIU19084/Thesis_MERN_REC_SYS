fa8bb520ce6f12ed69b2eec40cbb536a08291129
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2023-12-30 20:43:46 +0700

    init flask python api

--- Recommendation/flaskProject/app.py
+++ Recommendation/flaskProject/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'This is my first API call!'
 
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True)
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
 
-if __name__ == '__main__':
-    app.run()
+app.run(debug=True)
