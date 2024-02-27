b72084249f45e1ef99006e937a9956ac2bc4ef2a
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2024-02-27 08:51:32 +0700

    update categories dashboard

--- frontend/src/components/Admin/AllCategories.jsx
+++ frontend/src/components/Admin/AllCategories.jsx
@@ -429,7 +429,6 @@ const FormUpdate = ({ setOpenUpdate, detail }) => {
             <div>
               <input
                 type="submit"
-                value="Submit"
                 className="mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               />
             </div>
